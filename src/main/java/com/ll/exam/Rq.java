61a045529782b7cb7a3afef4150317f13d247281
sh970901 <aaddss639@naver.com>
2022-07-12 13:21:20 +0900

    해쉬맵 구현

--- src/main/java/com/ll/exam/Rq.java
+++ src/main/java/com/ll/exam/Rq.java
@@ -1,32 +1,55 @@
 package com.ll.exam;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
     String url;
+    String path;
+    Map<String, String> queryParams;
 
     public Rq(String url) {
         this.url = url;
-    }
+        String[] urlBits = url.split("\\?", 2);
+        this.path = urlBits[0];
 
-    public int getIntParam(String paramName, int defaultValue){
-        String[] urlBits =url.split("\\?",2);
-        if(urlBits.length == 1){
-            return defaultValue;
-        }
-        urlBits=urlBits[1].split("&");
+        queryParams = new HashMap<>();
 
-        for(String urlBit : urlBits){
-            String[] paramNameAndValue = urlBit.split("=",2);
-            String paramName_=paramNameAndValue[0];
-            String paramValue=paramNameAndValue[1];
+        if (urlBits.length == 2) {
+            String queryStr = urlBits[1];
 
-            if(paramName.equals(paramName_)){
-                return Integer.parseInt(paramValue);
+            String[] paramBits = queryStr.split("&");
+
+            for (String paramBit : paramBits) {
+                String[] paramNameAndValue = paramBit.split("=", 2);
+
+                if (paramNameAndValue.length == 1) {
+                    continue;
+                }
+
+                String paramName = paramNameAndValue[0].trim();
+                String paramValue = paramNameAndValue[1].trim();
+
+                queryParams.put(paramName, paramValue);
             }
         }
-        return defaultValue;
     }
-    public String getPath(){
-        String[] urlBits = url.split("\\?",2);
-        return urlBits[0];
+
+    public int getIntParam(String paramName, int defaultValue) {
+        if (queryParams.containsKey(paramName) == false) {
+            return defaultValue;
+        }
+
+        String paramValue = queryParams.get(paramName);
+
+        if (paramValue.length() == 0) {
+            return defaultValue;
+        }
+
+        return Integer.parseInt(paramValue);
+    }
+
+    public String getPath() {
+        return path;
     }
 }
