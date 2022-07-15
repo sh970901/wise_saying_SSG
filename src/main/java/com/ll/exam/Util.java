6beb82c62044a1cc302b2a0d72f0579a6eaf2daf
sh970901 <aaddss639@naver.com>
2022-07-15 15:43:46 +0900

    Pair생성

--- src/main/java/com/ll/exam/Util.java
+++ src/main/java/com/ll/exam/Util.java
@@ -118,9 +118,3 @@ public class Util {
     }
 }
 
-class Pair {
-    // Return a map entry (key-value pair) from the specified values
-    public static <T, U> Map.Entry<T, U> of(T first, U second) {
-        return new AbstractMap.SimpleEntry<>(first, second);
-    }
-}
